f10ecfb284fe4d32dd78dedf3688d7babd6e2bba
Kevin Toh <kevintoh0305@gmail.com>
2019-07-31 07:55:22 +0800

    Added categories but need fix the subtitle text overflow

---
+++ lib/components/horizontal_listview.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HorizontalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 100.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Category("images/categories/tshirt.png", "Shirt"),
+            Category("images/categories/dress.png", "Dress"),
+            Category("images/categories/jeans.png", "Pants"),
+            Category("images/categories/formal.png", "Formal"),
+            Category("images/categories/informal.png", "Informal"),
+            Category("images/categories/shoe.png", "Shoes"),
+            Category("images/categories/accessories.png", "Accessories"),
+          ],
+        ));
+  }
+}
+
+class Category extends StatelessWidget {
+  final String image_location;
+  final String image_caption;
+
+  Category(this.image_location, this.image_caption);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+          width: 100.0,
+          height: 80.0,
+          child: InkWell(
+            child: ListTile(
+              title: Image.asset(image_location),
+              subtitle: new Container(
+                alignment: Alignment.topCenter,
+                child: Text(image_caption),
+              ),
+            ),
+            onTap: () {},
+          )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,46 @@
+// Import Installed Packages 
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 
+// Import Local Components
+import 'package:shop_here/components/horizontal_listview.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    )
-  );
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
 }
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new _HomePageState();
   }
 }
 
-class _HomePageState extends State<HomePage>{
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     Widget image_carousel = new Container(
-      height: 300,
-      
-    );
+        height: 200.0,
+        child: new Carousel(
+          boxFit: BoxFit.cover,
+          images: [
+            AssetImage('images/c1.jpg'),
+            AssetImage('images/m1.jpeg'),
+            AssetImage('images/m2.jpg'),
+            AssetImage('images/w1.jpeg'),
+            AssetImage('images/w3.jpeg'),
+            AssetImage('images/w4.jpeg'),
+          ],
+          autoplay: false,
+          animationCurve: Curves.fastOutSlowIn,
+          animationDuration: Duration(milliseconds: 1000),
+          dotSize: 4.0,
+          indicatorBgPadding: 6.0,
+        )
+      );
 
     return new Scaffold(
       appBar: new AppBar(
@@ -33,13 +50,13 @@ class _HomePageState extends State<HomePage>{
         actions: <Widget>[
           new IconButton(
             icon: Icon(Icons.search, color: Colors.white),
-            onPressed: (){
+            onPressed: () {
               print("Search Button is pressed!");
             },
           ),
           new IconButton(
             icon: Icon(Icons.shopping_cart, color: Colors.white),
-            onPressed: (){
+            onPressed: () {
               print("Shopping Cart is pressed!");
             },
           )
@@ -54,75 +71,78 @@ class _HomePageState extends State<HomePage>{
               accountEmail: new Text("kevintoh0305@gmail.com"),
               currentAccountPicture: GestureDetector(
                 child: new CircleAvatar(
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, color: Colors.white, size: 40.0)
-                ),
+                    backgroundColor: Colors.grey,
+                    child: Icon(Icons.person, color: Colors.white, size: 40.0)),
               ),
-              decoration: new BoxDecoration(
-                color: Colors.red
-              ),
+              decoration: new BoxDecoration(color: Colors.red),
             ),
             // Body
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("Home"),
-                leading: Icon(Icons.home),
-                // onTap: () => {},
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("Home"),
+                  leading: Icon(Icons.home),
+                  // onTap: () => {},
+                ),
+                onTap: () => {}),
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("My Account"),
-                leading: Icon(Icons.person),
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("My Account"),
+                  leading: Icon(Icons.person),
+                ),
+                onTap: () => {}),
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("My Orders"),
-                leading: Icon(Icons.shopping_basket),
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("My Orders"),
+                  leading: Icon(Icons.shopping_basket),
+                ),
+                onTap: () => {}),
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("Categories"),
-                leading: Icon(Icons.dashboard),
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("Categories"),
+                  leading: Icon(Icons.dashboard),
+                ),
+                onTap: () => {}),
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("Favourites"),
-                leading: Icon(Icons.favorite, color: Colors.red),
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("Favourites"),
+                  leading: Icon(Icons.favorite, color: Colors.red),
+                ),
+                onTap: () => {}),
             Divider(),
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("Settings"),
-                leading: Icon(Icons.settings),
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("Settings"),
+                  leading: Icon(Icons.settings),
+                ),
+                onTap: () => {}),
             InkWell(
-              splashColor: Colors.redAccent,
-              child: ListTile(
-                title: Text("About"),
-                leading: Icon(Icons.help, color: Colors.blue),
-              ),
-              onTap: ()=>{}
-            ),
+                splashColor: Colors.redAccent,
+                child: ListTile(
+                  title: Text("About"),
+                  leading: Icon(Icons.help, color: Colors.blue),
+                ),
+                onTap: () => {}),
           ],
         ),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          // Image Carousel
+          image_carousel,         
+          // Padding with 'Categories' text
+          new Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: new Text("Categories"),
+          ),
+          // Horizontal List View for Categories
+          HorizontalList(),
+        ],
       ),
     );
   }
